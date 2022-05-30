5fe22be12978a1721b53cd8f8bc1463cdedb60d9
Aldana-Sigona, Jesus-Enrique <aldana.sigona@gmail.com>
2022-05-30 14:24:29 +0300

    Mapping last number of the lotto with list of restaurants (#2)
    Co-authored-by: Enrique Aldana <enrique.aldanasigona@cgi.com>

--- djangolottery/lotteryApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.views.generic import TemplateView
-
-from random import seed
-from random import randint
-
-class home(TemplateView):
-    template_name = "about.html"
